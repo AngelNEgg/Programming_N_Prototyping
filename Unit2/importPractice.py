ae8a6c0ce0d2df5c0bd8c7e67576084462d59ce3
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-09-23 12:35:07 -0400

    Update importPractice.py

--- Unit2/importPractice.py
+++ Unit2/importPractice.py
@@ -5,3 +5,14 @@ c = 2*r*math.pi
 a = math.pi*r**2
 
 print("C=" ,c," A=" ,a)
+-----------------------------------------------------------------------
+import math
+
+#r = radius, h = height
+r = int(input("What is the radius of your cylinder?"))
+h = int(input("What is the height of your cylinder?"))
+
+#v = volume
+v = math.pi*r**2*h
+
+print("The volume of your cylinder is",v)
