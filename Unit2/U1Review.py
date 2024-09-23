e2fb985a74edc7220fb20369d0664a8733d59e1e
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-09-23 11:53:18 -0400

    Create U1Review.py

---
+++ Unit2/U1Review.py
@@ -0,0 +1,13 @@
+'''
+	This program calculates the perimeter of a 
+  rectangle using input from the user.
+'''
+
+
+#Getting the length from the user
+length = int(input("What is the length of the rectangle?"))
+width = int(input("What is the width of the rectangle?"))
+
+perimeter = 2 * (length + width)
+
+print("the perimeter is " + str(perimeter))
