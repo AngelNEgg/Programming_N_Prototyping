a72f28e08ede18ffb0e2f7a05344559c26179182
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-09-26 12:01:55 -0400

    Create Math&RandomLibrary.py

---
+++ Unit2/Math&RandomLibrary.py
@@ -0,0 +1,18 @@
+import math
+
+import random
+
+# Completely random from 0 - 100
+x = random.randint(0,100)
+
+# Random from 0 - 100; only even numbers
+y = random.randrange(0,100,2)
+
+# Random from 0 - 100; only multiples of three
+z = random.randrange(0,100,3)
+
+print(x)
+
+print(y)
+
+print(z)
