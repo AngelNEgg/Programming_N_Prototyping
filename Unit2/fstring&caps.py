a2f864049c8429de4bb608f4a30a14f5f4b3e0f3
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-09-30 11:36:39 -0400

    Create fstring&caps.py

---
+++ Unit2/fstring&caps.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+
+name = name.capitalize() #	corrects capitalization of the string
+
+print(f"Welcome, {name}!")
+
+#	f-string, the f standing for format, allows for unique interractions for strings.
