834f14b3321fabc2f9ac1b7b50ba2a9ed85e0c2c
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-01 12:52:07 -0400

    Update moneyCount.py

--- Unit2/moneyCount.py
+++ Unit2/moneyCount.py
@@ -1,15 +1,15 @@
-#Code Complete
+#Lable
 
 a = int(input("How many pennies do you have?"))
 b = int(input("How many nickles do you have?"))
 c = int(input("How many dimes do you have?"))
 d = int(input("How many quarters do you have?"))
-e = int(input("How many loonies do you have?"))
-f = int(input("How many toonies do you have?"))
+e = int(input("How many loonies '($1)' do you have?"))
+f = int(input("How many toonies '($2)' do you have?"))
 
 tot = (a + (b*5) + (c*10) + (d*25) + (e*100) + (f*200))/100
 
-totDol = int(tot // 1)
+totDol = int((tot // 1))
         
 print("The number of pennies is " + str(a))
 print("The number of nickles is " + str(b))
@@ -18,6 +18,13 @@ print("The number of quarters is " + str(d))
 print("The number of loonies is " + str(e))
 print("The number of toonies is " + str(f))
 
-print("\nThe total amount of money in dollars is " + str(tot))
+print("\nThe total amount of money in dollars is $" + str(tot))
 
-print("\nAnother way to write this is $" + str(totDol) + ", " + str(f) + " toonies, " + str(e) + " loonies, " + str(d) + " quarters, " + str(c) + " dimes, " + str(b) + " nickles, and " + str(a) + " pennies.")
+change = tot - (tot // 1) # stores the value of the cents from the total amout of money
+
+chq = int(change // 0.25)
+chn = int((change - (change * 0.25)) // 0.10)
+chd = int((change - (change * 0.25) - (change * 0.10)) // 0.05)
+chp = int((change - (change * 0.25) - (change * 0.10) - (change * 0.05)) // 0.01)
+
+print(str(chq),str(chn),str(chd),str(chp))
