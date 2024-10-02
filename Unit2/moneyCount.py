3d895d336528407f0357f682fa5ad44e7e72af4b
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-02 06:47:25 -0400

    Update moneyCount.py

--- Unit2/moneyCount.py
+++ Unit2/moneyCount.py
@@ -23,8 +23,10 @@ print("\nThe total amount of money in dollars is $" + str(tot))
 change = tot - (tot // 1) # stores the value of the cents from the total amout of money
 
 chq = int(change // 0.25)
-chn = int((change - (change * 0.25)) // 0.10)
-chd = int((change - (change * 0.25) - (change * 0.10)) // 0.05)
+chd = int((change - (change * 0.25)) // 0.10)
+chn = int((change - (change * 0.25) - (change * 0.10)) // 0.05)
 chp = int((change - (change * 0.25) - (change * 0.10) - (change * 0.05)) // 0.01)
 
-print("You have", str(chq), "quarters,",str(chn),"nickles,",str(chd),"dimes, and",str(chp),"pennies in change alone.")
+dollars = int((tot - totDol))
+
+print("You have $" + str(totDol) + ",", str(chq), "quarters,",str(chn),"nickles,",str(chd),"dimes, and",str(chp),"pennies in change alone.")
