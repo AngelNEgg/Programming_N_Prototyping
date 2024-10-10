b8aa73c3ef93e8502fb4c13637ee15e203f6fb3f
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-10 12:50:07 -0400

    Update conditionals2.py

--- Unit 3/Practice/conditionals2.py
+++ Unit 3/Practice/conditionals2.py
@@ -1,3 +1,4 @@
+#IF and ELSE
 num = int(input("Enter a number: "))
 
 if num >= 5:
@@ -5,3 +6,15 @@ if num >= 5:
     
 else:
     print(str(num),"is less than or equal to 5!")
+-------------------------------------------------------------
+#IF, ELSE, and ELSE IF
+num = int(input("Enter a number: "))
+
+if num >= 1:
+    print(str(num),"is a positive number!")
+    
+elif num < 0:
+    print(str(num), "is a negative number...")
+    
+else:
+    print(str(num),"can't be positive or negative.")
