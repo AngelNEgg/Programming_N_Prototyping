2bcbc5d973eebca9fc7b40340ee897ed1999fb53
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-17 12:51:41 -0400

    Create CFU #9.py

---
+++ Unit 3/CFUs/CFU #9.py
@@ -0,0 +1,66 @@
+# complete rolls in conditionals
+
+import random
+
+rolls = int(input("How many rolls do you want to play? (1-5)"))
+
+num = random.randint(1,6)
+score = 0
+game = 1
+
+guess = int(input("Guess a number between 1 and 6: "))
+
+if guess == num and rolls >= 1:
+    print("Good job!")
+    score = score + 6
+    game = game + 2
+    num = random.randint(1,6)
+else:
+    print("Too bad...")
+    score = score - 1
+    game = game + 1
+    num = random.randint(1,6)
+
+if guess == num and game == 2 and rolls >= 1:
+    print("Good job!")
+    score = score + 6
+    game = game + 2
+    num = random.randint(1,6)
+else:
+    print("Too bad...")
+    score = score - 1
+    game = game + 1
+    num = random.randint(1,6)
+ 
+if guess == num and game == 3:
+    print("Good job!")
+    score = score + 6
+    game = game + 2
+    num = random.randint(1,6)
+else:
+    print("Too bad...")
+    score = score - 1
+    game = game + 1
+    num = random.randint(1,6)
+ 
+if guess == num and game == 4:
+    print("Good job!")
+    score = score + 6
+    game = game + 2
+    num = random.randint(1,6)
+else:
+    print("Too bad...")
+    score = score - 1
+    game = game + 1
+    num = random.randint(1,6)
+ 
+if guess == num and game == 5:
+    print("Good job!")
+    score = score + 6
+    game = game + 2
+else:
+    print("Too bad...")
+    score = score - 1
+    game = game + 1
+ 
+print("Your final score is...\n" + str(score))
