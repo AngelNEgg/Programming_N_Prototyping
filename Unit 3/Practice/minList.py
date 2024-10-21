5e4f6a852d3983963e6df196e96fea13536c4d80
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-21 11:06:53 -0400

    Create minList.py

---
+++ Unit 3/Practice/minList.py
@@ -0,0 +1,23 @@
+num1 = int(input("Enter a number: "))
+mini = [num1]
+print("Smallest:",min(mini))
+
+num2 = int(input("Enter a number: "))
+mini = [num1,num2]
+print("Smallest:",min(mini))
+
+num3 = int(input("Enter a number: "))
+mini = [num1,num2,num3]
+print("Smallest:",min(mini))
+
+num4 = int(input("Enter a number: "))
+mini = [num1,num2,num3,num4]
+print("Smallest:",min(mini))
+
+num5 = int(input("Enter a number: "))
+mini = [num1,num2,num3,num4,num5]
+print("Smallest:",min(mini))
+
+num6 = int(input("Enter a number: "))
+mini = [num1,num2,num3,num4,num5,num6]
+print("Smallest:",min(mini))
