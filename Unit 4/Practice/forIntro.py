6c00341687d6e1a6f1a467c95fbdd44b4e15d377
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-25 11:59:45 -0400

    Update forIntro.py

--- Unit 4/Practice/forIntro.py
+++ Unit 4/Practice/forIntro.py
@@ -1,4 +1,13 @@
-# For-Loop Introduction
+# For-Loop Introduction 1
 
 for x in range(5):
     print(x)
+# -----------------------------------
+# For-Loop Introduction 2
+
+x = 10
+
+for i in range(10,20):
+    print(f"Loop #{i-10}, x = {x}")
+    x = x + 1
+# -----------------------------------
