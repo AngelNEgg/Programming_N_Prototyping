486420926f154417d34272fa5ecc50521033e12a
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-25 12:20:31 -0400

    Update forIntro.py

--- Unit 4/Practice/forIntro.py
+++ Unit 4/Practice/forIntro.py
@@ -11,3 +11,16 @@ for i in range(10,20):
     print(f"Loop #{i-10}, x = {x}")
     x = x + 1
 # -----------------------------------
+# For-Loop Introduction 3
+
+x = 0
+
+for i in range(5):
+    print(x + 1)
+for i in range(1,4):
+    print(x - 2)
+for i in range(2,19,3):
+    print(x + 3)
+for i in range(0,100,5):
+    x = x + 5
+    print(x)
