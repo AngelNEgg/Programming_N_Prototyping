34f6f4977c50fc2a3d1793a73dd2fcd9daa800a6
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-25 12:35:43 -0400

    Update CFU #11.py

--- Unit 4/CFUs/CFU #11.py
+++ Unit 4/CFUs/CFU #11.py
@@ -1,1 +1,5 @@
+x = 100
 
+for i in range(1,100,1):
+    x -= 1
+    print(f"{x} bottles of beer on the wall!\n{x} bottles of beer!\nTake one down, pass it around!\n{x - 1} bottles of beer on the wall!\n")
