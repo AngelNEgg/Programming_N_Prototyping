c9ee2b68711c90d1d0da43540a316bd850110cd5
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-10-30 12:13:36 -0400

    Update CFU #12.py

--- Unit 4/CFUs/CFU #12.py
+++ Unit 4/CFUs/CFU #12.py
@@ -1,45 +1,45 @@
+
 #Angel Nazaire
-#10/21/2024
+#10/29/2024
 #Period 5-6
 # --------------------------------------------------------------------------------------------------
-#Part 1
 
 '''Create a loop that asks the user for a password,
-denies them if it doesn't match "simonsnyc", and ends if it does.'''
+denies them if it doesn't match "simonsnyc", and ends if it does*.
+(*In version 2, this action will be counted up to three times. Afterwards, the loop will end.)'''
+
+userPass = "placeholder"
+version = int(input("Which version do you want to use?(1 or 2)"))
+
+def userInput(x):
+    x = input("What is the password?")
+    return x
 
 passw = "simonsnyc"
-userPass = input("What is the password? ")
-
-while userPass != passw:
-    print("Wrong Password!")
-    userPass = input("What is the password?")
-        
-print("Correct! You may pass..")
-# --------------------------------------------------------------------------------------------------
-#Part 2
-
-'''Create a loop that asks the user for a password,
-denies them if it doesn't match "simonsnyc", and ends if it does.'''
-
-passw = "simonsnyc"
-userPass = input("What is the password? ")
 i = 0
 
-# ver1_Func: x = userPass, y = passw
-# ver2_Func: x = userPass, y = passw, z = i
-
-def ver1_Func(x,y)
-	while userPass != passw:
-    	print("Wrong Password!")
-    	userPass = input("What is the password?")
+def ver1_Func(x,y):
+    userInput(userPass)
+    while x != y:
+        print("Wrong Password!")
+        userInput(userPass)
         
-	print("Correct! You may pass..")
+    print("Correct! You may pass..")
     
-def ver2_Func(x,y,z)
-for i in range(3):
-	while userPass != passw:
-    	print("Wrong Password!")
-        i += 1
-    	userPass = input("What is the password?")
+def ver2_Func(x,y,z):
+    userInput(userPass)
+    for z in range(3):
+        while x != y:
+            print("Wrong Password!")
+            z += 1
+            userInput(userPass)
         
-	print("Correct! You may pass..")
+        print("Correct! You may pass..")
+
+if version == 1:
+    print("Starting version 1...\n")
+    ver1_Func(userPass,passw)
+elif version == 2:
+    print("Starting version 2...\n")
+    ver2_Func(userPass,passw,i)
+ 
