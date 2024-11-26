f5607ca40fd0de8092439781ea60c8541ddd4e7c
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-11-26 12:42:09 -0500

    Update CFU #14.py

--- Unit 6/CFU #14.py
+++ Unit 6/CFU #14.py
@@ -2,12 +2,11 @@
 
 import simplegui
 
-def draw_handler(canvas):
-    
+def draw(canvas):
+    canvas.draw_line([50,10], [150,10], 4, "White")
 
-# library.create_frame("name of canvas", width, height)
+frame = simplegui.create_frame("Home", 200, 200)
+frame.set_draw_handler(draw)
 
-frame = simplegui.create_frame("CFU #13: Happy Dots", 200, 200)
-frame.set_canvas_background("Black")
-frame.set_draw_handler(draw_handler(frame,one,five,num))
 frame.start()
+
