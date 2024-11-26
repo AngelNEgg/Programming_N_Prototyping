38351a0406c7d043ef56e32155ec5568f1b5c238
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-11-26 12:49:23 -0500

    Update CFU #14.py

--- Unit 6/CFU #14.py
+++ Unit 6/CFU #14.py
@@ -4,9 +4,10 @@ import simplegui
 
 def draw(canvas):
     canvas.draw_line([50,10], [150,10], 4, "White")
+    
+    canvas.draw_line([50,20],[50,150],4,"White")
 
 frame = simplegui.create_frame("Home", 200, 200)
 frame.set_draw_handler(draw)
 
 frame.start()
-
