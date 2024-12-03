05a825b054f0b781d27a668e8a98fbdb97b9a7fd
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-12-03 11:52:33 -0500

    Update proj.py

--- Unit 6/proj.py
+++ Unit 6/proj.py
@@ -1,18 +1,14 @@
-# CodeSkulptor runs Python programs in your browser.
-# Click the upper left button to run this simple demo.
-
-#ffe9ab
-
-
 # Angel Nazaire
 # 12/2/2024
 # Period 5-6
+'''Create a Thanksgiving themed image using your current knowledge from the "SimpleGUI" library.'''
 
 import simplegui
 import random
 
 xmax = 600
 ymax = 400
+rand = random.randint(1,5)
 
 # ymax = 2/3 of xmax
 
@@ -20,14 +16,24 @@ def draw(canvas):
     #BG (SCALE GRADIENT)
     def bg(canvas):
         canvas.draw_circle((xmax/2,ymax/2), xmax, xmax, "#ffaf47", "#ffaf47")
-        canvas.draw_line([0,30],[xmax,30], 100, "#ff9233")
-        canvas.draw_line([0,1],[xmax,1], 50, "#ff6c1c")
-        canvas.draw_circle((0,0), 50, 5, "#ffbb00", "#ffe9ab")
+        canvas.draw_line([0,30],[xmax,30], 200, "#ff9233")
+        canvas.draw_line([0,1],[xmax,1], 100, "#ff6c1c")
+        canvas.draw_circle((0,0), 100, 5, "#ffbb00", "#ffe9ab")
+        
+        #-tree
+        canvas.draw_line([0,0],[0,ymax], 50, "#634824")
+        canvas.draw_line([0,0],[0,ymax], 35, "#301300")
+        canvas.draw_line([-10,(ymax/2)],[(xmax/2)-200,ymax], 50, "#301300")
+        
+        #-sky
+        canvas.draw_polygon([(50,45),(250,45),(100,15),(75,15)], 3, "rgba(255, 150, 38, 0.5)", "rgba(255, 213, 74, 0.5)")
+        canvas.draw_polygon([(400,50),(550,25),(575,0),(600,0),(600,50)], 3, "rgba()", "rgba()")
     
     #ground
     def ground(canvas):
         canvas.draw_line([0,ymax],[xmax,ymax], 220, "#1A9241")
         canvas.draw_line([0,ymax],[xmax,ymax], 170, "#136D30")
+        canvas.draw_circle((315,360),90,20, "#136D30", "#136D30")
     
     #pumpkin (MOVE DOWN TO GROUND)
     #-stem (SHORTEN)
@@ -35,23 +41,26 @@ def draw(canvas):
         canvas.draw_line([xmax/2,ymax/2],[xmax/2,30], 40, "Green")
         canvas.draw_line([(xmax/2)+5,ymax/2],[(xmax/2)+5,30], 35, "rgb(0,75,25)")
     
-    #-shell (REPOSITION)
+    #-shell (complete)
     def shell(canvas):
         canvas.draw_circle(((xmax/2)-40,(ymax/2)+10), 90, 4, "#B55400", "#FFAB00")
         canvas.draw_circle(((xmax/2)+40,(ymax/2)+10), 90, 4, "#B55400", "#FFAB00")
         canvas.draw_circle((xmax/2,ymax/2), 100, 5, "#B55400", "#FFAB00")
     
-    #-shading (complete)
+    #-overlay (complete)
     def shade(canvas):
+        #-shading
         canvas.draw_circle(((xmax/2)+5,(ymax/2)+6), 95, 5, "rgba(0,0,0,0)", "#AD7400")
         canvas.draw_circle(((xmax/2)-40,(ymax/2)+16), 85, 4, "rgba(0,0,0,0)", "#AD7400")
         canvas.draw_circle((((xmax/2)+45,(ymax/2)+16)), 85, 4, "rgba(0,0,0,0)", "#AD7400")
+        
         #-darker
         canvas.draw_circle(((xmax/2)+5,(ymax/2)+16), 85, 5, "rgba(0,0,0,0)", "#824800")
         canvas.draw_circle(((xmax/2)-42,(ymax/2)+26), 75, 4, "rgba(0,0,0,0)", "#824800")
         canvas.draw_circle((((xmax/2)+47,(ymax/2)+26)), 75, 4, "rgba(0,0,0,0)", "#824800")
         
         #-leaves (RANDOM) (IMPLEMENT)
+         
         
         #-filter (complete)
         canvas.draw_circle((xmax/2,ymax/2), xmax, xmax, "rgba(135,72,0,0)", "rgba(135,72,0,0.25)")
