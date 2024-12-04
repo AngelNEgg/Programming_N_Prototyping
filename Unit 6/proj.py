5f97281b5114370c34df7b13be2492b77e0dfa3e
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-12-04 12:14:29 -0500

    Update proj.py

--- Unit 6/proj.py
+++ Unit 6/proj.py
@@ -8,9 +8,6 @@ import random
 
 xmax = 600
 ymax = 400
-rand = random.randint(1,5)
-
-# ymax = 2/3 of xmax
 
 def draw(canvas):
     #BG (complete)
@@ -20,16 +17,16 @@ def draw(canvas):
         canvas.draw_line([0,1],[xmax,1], 100, "#ff6c1c")
         canvas.draw_circle((0,0), 100, 5, "#ffbb00", "#ffe9ab")
         
-        #-tree
+        #-tree (complete)
         canvas.draw_line([0,0],[0,ymax], 50, "#634824")
         canvas.draw_line([0,0],[0,ymax], 35, "#301300")
         canvas.draw_line([-10,(ymax/2)],[(xmax/2)-200,ymax], 50, "#301300")
         
-        #-sky
+        #-sky (complete)
         canvas.draw_polygon([(50,45),(250,45),(100,15),(75,15)], 3, "rgba(255, 150, 38, 0.5)", "rgba(255, 213, 74, 0.5)")
         canvas.draw_polygon([(400,50),(550,25),(575,0),(600,0),(600,50)], 3, "rgba()", "rgba()")
     
-    #ground
+    #ground (complete)
     def ground(canvas):
         canvas.draw_line([0,ymax],[xmax,ymax], 220, "#1A9241")
         canvas.draw_line([0,ymax],[xmax,ymax], 170, "#136D30")
@@ -59,12 +56,12 @@ def draw(canvas):
         canvas.draw_circle(((xmax/2)-40,(ymax/2)+16), 85, 4, "rgba(0,0,0,0)", "#AD7400")
         canvas.draw_circle((((xmax/2)+45,(ymax/2)+16)), 85, 4, "rgba(0,0,0,0)", "#AD7400")
         
-        #-darker
+        #-darker (complete)
         canvas.draw_circle(((xmax/2)+5,(ymax/2)+16), 85, 5, "rgba(0,0,0,0)", "#824800")
         canvas.draw_circle(((xmax/2)-42,(ymax/2)+26), 75, 4, "rgba(0,0,0,0)", "#824800")
         canvas.draw_circle((((xmax/2)+47,(ymax/2)+26)), 75, 4, "rgba(0,0,0,0)", "#824800")
         
-        #-particles
+        #-particles (complete)
         for i in range(1,1000):
             
             r = random.randint(0,255)
@@ -78,19 +75,19 @@ def draw(canvas):
 
             canvas.draw_point((x, y), randRGBColor)
         
-        #-leaves (RANDOM) (REWRITE)
-        for i in range(1,100):
+        #-leaves (complete)
+        for i in range(1,10):
             
             r = random.randint(0,255)
             g = random.randint(105,125)
             b = 0
 
-            randRGBColor = "RGB( " + str(r) + "," + str(g) + "," + str(b) + ")"     
+            randRGBColor = "RGB( " + str(r) + "," + str(g) + "," + str(b) + ")"    
 
             x = random.randint(2, xmax)
-            y = random.randint(2, xmax)
+            y = random.randint(2, ymax)
 
-            canvas.draw_polygon((x, y), randRGBColor)
+            canvas.draw_polygon([(x, y),((x+5), (y+5)),((x+10), (y+5)),((x+5), y)], 3, randRGBColor)
          
         
         #-filter (complete)
