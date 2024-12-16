ea91cceef320a02ec1e2f65a61f9b93fab35026b
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2024-12-16 12:51:27 -0500

    Update Animation.py

--- Unit 6/Animation.py
+++ Unit 6/Animation.py
@@ -3,7 +3,7 @@
 # Period 5-6
 '''Create an animation themed around winter holidays using your current knowledge of SimpleGUI and Python.'''
 
-
+# Road color: #7e838f
 import simplegui
 import random
 
@@ -12,8 +12,8 @@ height = 400
 Hwidth = width/2
 Hheight = height/2
 
-snowNum = 5
-snowSpeed = 30
+snowNum = 50
+snowSpeed = 1
 
 snowY = []
 
@@ -27,11 +27,17 @@ def draw(canvas):
     global snowY
     # BG + Ground
     canvas.draw_polygon([(0,0),(0,height),(width,height),(width,0)], 4, "#0c1b3b", "#0c1b3b")
-    canvas.draw_polygon([(0,Hheight),(0,height),(width,height),(width,Hheight)], 4, "rgb(191, 212, 255)", "rgb(191, 212, 255)")
+    canvas.draw_polygon([(0,Hheight),(0,height),(width,height),(width,Hheight)], 4, "#ebf1ff", "#ebf1ff")
+    canvas.draw_polygon([(Hwidth,Hheight),(Hwidth/2,height),((Hwidth/2)*3,height)], 2, "#7e838f", "#7e838f")
     
-    # House (SET POSITION)
-    canvas.draw_polygon([(),(),(),()], 2, "#c2451b", "#c2451b")
+    # House (FINISH STYLE)
+    canvas.draw_polygon([(Hwidth-57,Hheight),(Hwidth+57,Hheight),(Hwidth+57,Hheight+75),(Hwidth-57,Hheight+75)], 4, "#701911", "#c2451b")
+    canvas.draw_polygon([(Hwidth-57,Hheight),(Hwidth+57,Hheight),(Hwidth,Hheight-75)], 4, "#596263", "#ffffff")
     
+    # Filter
+    canvas.draw_polygon([(0,0),(0,height),(width,height),(width,0)], 2, "rgba(82, 106, 156, 0.5)", "rgba(82, 106, 156, 0.5)")
+    
+    # Snow
     for i in range(snowNum):
         snowX = random.randint(0,width)
         canvas.draw_circle((snowX,snowY[i]), 3, 2, "rgba(191, 212, 255, 0.25)", "rgba(255, 255, 255, 0.75)")
