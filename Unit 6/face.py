0185e785a657947c71a2cc81b0c6f614d46f44cc
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2025-01-09 11:36:16 -0500

    Update face.py

--- Unit 6/face.py
+++ Unit 6/face.py
@@ -1,13 +1,13 @@
 import simplegui
 
-xmax = 600
-ymax = 600
+xmax = 200
+ymax = 200
 
 def draw(canvas):
-    canvas.draw_circle((xmax/2,ymax/2), 100, 10, "#fcb503", "#fcd703")
+    canvas.draw_circle((xmax/2,ymax/2), 75, 10, "#fcb503", "#fcd703")
     canvas.draw_circle((xmax/2-25,ymax/2-25), 5, 10, "Black")
     canvas.draw_circle((xmax/2+25,ymax/2-25), 5, 10, "Black")
-    canvas.draw_line((xmax/2-50,ymax/2+25),(xmax/2+50,ymax/2+25), 5, "brown")
+    canvas.draw_line((xmax/2-25,ymax/2+25),(xmax/2+25,ymax/2+25), 5, "brown")
 
 frame = simplegui.create_frame("Home", xmax, ymax)
 frame.set_draw_handler(draw)
