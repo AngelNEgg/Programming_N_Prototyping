81faf61bf95317c648babb546d2d629878ef4d42
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2025-03-06 12:51:54 -0500

    Create PCC #4.py

---
+++ Practice/PCC #4.py
@@ -0,0 +1,57 @@
+# Angel Nazaire
+# 3/5/2025
+# Period 5-6
+
+'''Python Code Challenge 4: '''
+
+def gridFunc1(x,y):
+    x()
+    y()
+    y()
+    y()
+    y()
+    x()
+    y()
+    y()
+    y()
+    y()
+    x()
+
+def gridFunc2(x,y):
+    x()
+    y()
+    y()
+    y()
+    y()
+    x()
+    y()
+    y()
+    y()
+    y()
+    x()
+    y()
+    y()
+    y()
+    y()
+    x()
+    y()
+    y()
+    y()
+    y()
+    x()
+
+def plusLine1():
+    print('+','-','-','-','-','+','-','-','-','-','+')
+    
+def plusLine2():
+    print('+','-','-','-','-','+','-','-','-','-','+','-','-','-','-','+','-','-','-','-','+')
+    
+def dashLine1():
+    print('|',' ',' ',' ',' ','|',' ',' ',' ',' ','|')
+    
+def dashLine2():
+    print('|',' ',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|',' ',' ',' ',' ','|')
+    
+    
+gridFunc1(plusLine1,dashLine1)
+gridFunc2(plusLine2,dashLine2)
