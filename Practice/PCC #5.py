e13acf46569217dda2d67ecb634bab15300fbcd5
Angel N. <144722372+AngelNEgg@users.noreply.github.com>
2025-03-07 11:20:10 -0500

    Update PCC #5.py

--- Practice/PCC #5.py
+++ Practice/PCC #5.py
@@ -1,1 +1,15 @@
+# Angel Nazaire
+# 3/7/2025
+# Period 5-6
+'''Python Code Challenge #5: '''
+
 def check_fermat(a,b,c,n):
+  ans1 = a**n
+  ans2 = b**n
+  ans3 = c**n
+  if n>2 and (ans1+ans2) == ans3:
+    print("Holy smokes, Fermat was wrong!")
+  else:
+    print("No, that doesn't work.")
+
+check_fermat(8,6,3,2)
